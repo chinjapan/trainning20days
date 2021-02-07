e85fd2ab8cfd99501472fee90e3f73466ef970ab
vovanbechin <bechin.vovan@gmail.com>
2021-02-07 22:29:03 +0900

    20210207 22:28 backend done

---
+++ Backend/bookstore/src/main/java/com/bookstore/controller/bookinforcontroller.java
@@ -0,0 +1,78 @@
+package com.bookstore.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bookstore.extension.ResourceNotFoundException;
+import com.bookstore.model.bookinfor;
+import com.bookstore.repository.bookinforrepository;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class bookinforcontroller {
+	
+	@Autowired
+	private bookinforrepository bookinforepository ;
+	
+	//get all bookinfo
+	@GetMapping("/getallbookinfo")
+	public List<bookinfor> getallbookinfo(){
+		return bookinforepository.findAll();
+	}
+	
+	//Create bookinfo
+	@PostMapping("/createbookinfo")
+	public bookinfor createbookinfo(@RequestBody bookinfor bookinfo) {
+		return bookinforepository.save(bookinfo);
+	}
+	
+	//Get Bookinfo by Id rest api
+	@GetMapping("/bookinfo/{id}")
+	public ResponseEntity<bookinfor> getbookinfobyid(@PathVariable Long id) {
+		bookinfor bookinfo = bookinforepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Bookinfo not exist with: " + id));
+		return ResponseEntity.ok(bookinfo);
+	}
+	
+	//update bookinfo
+		@PutMapping("/updatebookinfo/{id}")
+		public ResponseEntity<bookinfor> updatebookinfo(@PathVariable Long id, @RequestBody bookinfor bookinfodetail) {
+			bookinfor bookinfo = bookinforepository.findById(id)
+					.orElseThrow(() -> new ResourceNotFoundException("Bookinfo not exist with: " + id));
+			
+			bookinfo.setBookname(bookinfodetail.getBookname());
+			bookinfo.setCompanyname(bookinfodetail.getCompanyname());
+			bookinfo.setCost(bookinfodetail.getCost());
+			bookinfo.setPagenumber(bookinfodetail.getPagenumber());
+			
+			bookinfor updatebookinfo = bookinforepository.save(bookinfo);
+			return ResponseEntity.ok(updatebookinfo);
+		}
+		
+		//delete bookinfo	
+		@DeleteMapping("/deletebookinfo/{id}")
+		public ResponseEntity<Map<String, Boolean>> deletebookinfo(@PathVariable Long id){
+			bookinfor bookinfo = bookinforepository.findById(id)
+					.orElseThrow(() -> new ResourceNotFoundException("Bookinfo not exist with: " + id));
+			
+			bookinforepository.delete(bookinfo);
+			Map<String, Boolean> response = new HashMap<>();
+			response.put("deleted", Boolean.TRUE);
+			
+			return ResponseEntity.ok(response);
+			
+		}
+
+}
---
+++ Backend/bookstore/src/main/java/com/bookstore/model/bookinfor.java
@@ -0,0 +1,80 @@
+package com.bookstore.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="bookstore")
+public class bookinfor {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long Id;
+	
+	@Column(name = "bookname")
+	private String bookname;
+	
+	@Column(name = "companyname")
+	private String companyname;
+	
+	@Column(name = "createdate")
+	private String createdate;
+	
+	@Column(name = "cost")
+	private long cost;
+	
+	@Column(name = "pagenumber")
+	private long pagenumber;
+	
+	public bookinfor() {
+		
+	}
+	public bookinfor(String bookname, String companyname, String createdate, long cost, long pagenumber) {
+		super();
+		this.bookname = bookname;
+		this.companyname = companyname;
+		this.createdate = createdate;
+		this.cost = cost;
+		this.pagenumber = pagenumber;
+	}
+	public long getId() {
+		return Id;
+	}
+	public void setId(long Id) {
+		this.Id = Id;
+	}
+	public String getBookname() {
+		return bookname;
+	}
+	public void setBookname(String bookname) {
+		this.bookname = bookname;
+	}
+	public String getCompanyname() {
+		return companyname;
+	}
+	public void setCompanyname(String companyname) {
+		this.companyname = companyname;
+	}
+	public String getcreatedate() {
+		return createdate;
+	}
+	public void createdate(String createdate) {
+		this.createdate = createdate;
+	}
+	public long getCost() {
+		return cost;
+	}
+	public void setCost(long cost) {
+		this.cost = cost;
+	}
+	public long getPagenumber() {
+		return pagenumber;
+	}
+	public void setPagenumber(long pagenumber) {
+		this.pagenumber = pagenumber;
+	}
+}
